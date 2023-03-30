5befecd364ad70748a2a707898b120643dae8fd8
NemaDev <petermannfelipe@gmail.com>
2023-03-30 00:43:44 -0300

    rename client to main

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,11 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Nema#0001'
+name 'nema_pickupbike'
+author 'ameN#0001'
 description 'nema pickupbike'
 version '1.1.0'
-lua54 'yes'
+
 use_experimental_fxv2_oal 'yes'
 
 client_script {
@@ -12,3 +13,5 @@ client_script {
     'config.lua',
     'client/client.lua'
 }
+
+lua54 'yes'
